15319802742661fd022612779305def3944e3695
joaberch <122774888+joaberch@users.noreply.github.com>
2024-04-16 13:27:54 +0200

    Resolve error with component

--- website/components/task.js
+++ website/components/task.js
@@ -1,22 +1,15 @@
 app.component('task-display', {
-    props: {
-        Lists: {
-            required: true
-        }
-    },
     template:
         /*html*/
         `<div class="task">
             <table>
                 <tr>
-                    <td v-for="(x, index) in Lists">
-                        <div v-if="!Lists[index].done">
-                            {{ x.name }} - {{ x.priority }}
-                            <br>
-                                {{ x.description }}
-                                <br>
-                                <button type="button" @click="taskDone(index)">Done</button>
-                        </div>
+                    <td v-for="(x, index) in todoLists">
+                        {{ x.name }} - {{ x.priority }}
+                        <br>
+                        {{ x.description }}
+                        <br>
+                        <button type="button" @click="taskDone(index)">Done</button>
                     </td>
                 </tr>
             </table>
@@ -24,41 +17,40 @@ app.component('task-display', {
     data() {
         return {
             selectedTask: 0,
-            Lists: [
-                { name: 'Manger', description: 'Manger une pomme', priority: 60, done: true },
-                { name: 'Boire', description: 'Boire de l\'eau', priority: 9, done: false },
-                { name: 'Respirer', description: '...', priority: 10, done: false },
-                { name: 'Cuisiner', description: 'Cuisiner une pomme', priority: 3, done: false },
-                { name: 'Regarder', description: '...', priority: 1, done: false },
-                { name: 'Manger', description: 'Manger une pomme', priority: 6, done: false },
-                { name: 'Boire', description: 'Boire de l\'eau', priority: 9, done: false },
-                { name: 'Respirer', description: '...', priority: 10, done: false },
-                { name: 'Cuisiner', description: 'Cuisiner une pomme', priority: 3, done: false },
-                { name: 'Regarder', description: '...', priority: 1, done: false },
-                { name: 'Manger', description: 'Manger une pomme', priority: 6, done: false },
-                { name: 'Boire', description: 'Boire de l\'eau', priority: 9, done: false },
-                { name: 'Respirer', description: '...', priority: 10, done: false },
-                { name: 'Cuisiner', description: 'Cuisiner une pomme', priority: 3, done: false },
-                { name: 'Regarder', description: '...', priority: 1, done: false },
-                { name: 'Manger', description: 'Manger une pomme', priority: 6, done: false },
-                { name: 'Boire', description: 'Boire de l\'eau', priority: 9, done: false },
-                { name: 'Respirer', description: '...', priority: 10, done: false },
-                { name: 'Cuisiner', description: 'Cuisiner une pomme', priority: 3, done: false },
-                { name: 'Regarder', description: '...', priority: 1, done: false },
+            todoLists: [
+                { name: 'Manger', description: 'Manger une pomme', priority: 6},
+                { name: 'Boire', description: 'Boire de l\'eau', priority: 9},
+                { name: 'Respirer', description: '...', priority: 10},
+                { name: 'Cuisiner', description: 'Cuisiner une pomme', priority: 3},
+                { name: 'Regarder', description: '...', priority: 1},
+                { name: 'Manger', description: 'Manger une pomme', priority: 6},
+                { name: 'Boire', description: 'Boire de l\'eau', priority: 9},
+                { name: 'Respirer', description: '...', priority: 10},
+                { name: 'Cuisiner', description: 'Cuisiner une pomme', priority: 3},
+                { name: 'Regarder', description: '...', priority: 1},
+                { name: 'Manger', description: 'Manger une pomme', priority: 6},
+                { name: 'Boire', description: 'Boire de l\'eau', priority: 9},
+                { name: 'Respirer', description: '...', priority: 10},
+                { name: 'Cuisiner', description: 'Cuisiner une pomme', priority: 3},
+                { name: 'Regarder', description: '...', priority: 1},
+                { name: 'Manger', description: 'Manger une pomme', priority: 6},
+                { name: 'Boire', description: 'Boire de l\'eau', priority: 9},
+                { name: 'Respirer', description: '...', priority: 10},
+                { name: 'Cuisiner', description: 'Cuisiner une pomme', priority: 3},
+                { name: 'Regarder', description: '...', priority: 1},
             ],
+            doneLists: []
         }
     },
     computed: {
-        checkDone() {
-            return this.Lists[this]
-        },
         updateSelectedTask(index) {
             this.selectedTask = index
         },
     },
     methods: {
         taskDone(index) {
-            this.Lists[index].done = true
+            this.doneLists.push(this.todoLists[index]);
+            this.todoLists.splice(index, 1);
         }
     }
 })
--- website/main.js
+++ website/main.js
@@ -2,41 +2,16 @@ const app = Vue.createApp({
     data() {
         return {
             selectedTask: 0,
-            Lists: [
-                {name: 'Manger', description: 'Manger une pomme', priority: 60, done: true},
-                {name: 'Boire', description: 'Boire de l\'eau', priority: 9, done: false},
-                {name: 'Respirer', description: '...', priority: 10, done: false},
-                {name: 'Cuisiner', description: 'Cuisiner une pomme', priority: 3, done: false},
-                {name: 'Regarder', description: '...', priority: 1, done: false},
-                {name: 'Manger', description: 'Manger une pomme', priority: 6, done: false},
-                {name: 'Boire', description: 'Boire de l\'eau', priority: 9, done: false},
-                {name: 'Respirer', description: '...', priority: 10, done: false},
-                {name: 'Cuisiner', description: 'Cuisiner une pomme', priority: 3, done: false},
-                {name: 'Regarder', description: '...', priority: 1, done: false},
-                {name: 'Manger', description: 'Manger une pomme', priority: 6, done: false},
-                {name: 'Boire', description: 'Boire de l\'eau', priority: 9, done: false},
-                {name: 'Respirer', description: '...', priority: 10, done: false},
-                {name: 'Cuisiner', description: 'Cuisiner une pomme', priority: 3, done: false},
-                {name: 'Regarder', description: '...', priority: 1, done: false},
-                {name: 'Manger', description: 'Manger une pomme', priority: 6, done: false},
-                {name: 'Boire', description: 'Boire de l\'eau', priority: 9, done: false},
-                {name: 'Respirer', description: '...', priority: 10, done: false},
-                {name: 'Cuisiner', description: 'Cuisiner une pomme', priority: 3, done: false},
-                {name: 'Regarder', description: '...', priority: 1, done: false},
-            ],
         }
     },
     computed: {
-        checkDone() {
-            return this.Lists[this]
-        },
         updateSelectedTask(index) {
             this.selectedTask = index
         },
     },
     methods: {
         taskDone(index) {
-            this.Lists[index].done = true
+            this.doneLists[index].done = true
         },
         fillTable(data) {
             const tableBody = document.querySelector('#heritageTable tbody');
